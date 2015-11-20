72deb68451ddbd4f56112fa329f46af29715dce4
Simon Baynes <baynezy@gmail.com>
2015-11-20 04:31:35 +0000

    Added home page

---
+++ test/Html2MarkdownConverter.Web.Test/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Html2MarkdownConverter.Web.Controllers;
+using NUnit.Framework;
+
+namespace Html2MarkdownConverter.Web.Test.Controllers
+{
+	[TestFixture]
+	class HomeControllerTest
+	{
+		[Test]
+		public void Index_WhenCalled_ThenReturnViewResult()
+		{
+			var controller = CreateController();
+
+			var result = controller.Index() as ViewResult;
+
+			Assert.That(result, Is.Not.Null);
+		}
+
+		private static HomeController CreateController()
+		{
+			return new HomeController();
+		}
+	}
+}
