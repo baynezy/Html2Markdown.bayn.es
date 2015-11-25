0de979d3723ea7a7fbfc87542b9949ac4a1da131
Simon Baynes <baynezy@gmail.com>
2015-11-25 04:28:51 +0000

    Parking what I have

---
+++ src/Html2MarkdownConverter.Web/Content/js/copy-to-clipboard.js
@@ -0,0 +1,28 @@
+﻿var clipboard = {
+	init: function () {
+		this.loadEvents();
+	},
+	
+	loadEvents : function() {
+		$("button#btnCopyToClipboard").on("click", function() {
+			clipboard.copyToClipboard();
+		});
+	},
+	
+	copyToClipboard: function () {
+		var copyTextarea = document.querySelector("#markdown");
+		copyTextarea.select();
+		
+		try {
+			var successful = document.execCommand("copy");
+			var msg = successful ? 'successful' : 'unsuccessful';
+			console.log('Copying text command was ' + msg);
+		} catch (err) {
+			console.log('Oops, unable to copy');
+		}
+	}
+};
+
+$(document).ready(function() {
+	clipboard.init();
+});
