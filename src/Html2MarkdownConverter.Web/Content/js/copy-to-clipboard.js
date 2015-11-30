62441bc328fdcbedd61f11bf2be2f1af456711c5
Simon Baynes <baynezy@gmail.com>
2015-11-30 05:34:40 +0000

    Parking what I have

--- src/Html2MarkdownConverter.Web/Content/js/copy-to-clipboard.js
+++ src/Html2MarkdownConverter.Web/Content/js/copy-to-clipboard.js
@@ -1,30 +1,3 @@
-﻿var clipboard = {
-	init: function () {
-		var client = new ZeroClipboard($("button#btnCopyToClipboard"));
-	},
-	
-	loadEvents : function() {
-		var copyTextareaBtn = document.querySelector("button#btnCopyToClipboard");
-
-		copyTextareaBtn.addEventListener("click", function (event) {
-			clipboard.copyToClipboard();
-		});
-	},
-	
-	copyToClipboard: function () {
-		var copyTextarea = document.querySelector("#markdown");
-		copyTextarea.select();
-
-		try {
-			var successful = document.execCommand("copy");
-			var msg = successful ? "successful" : "unsuccessful";
-			console.log("Copying text command was " + msg);
-		} catch (err) {
-			console.log("Oops, unable to copy");
-		}
-	}
-};
-
-$(document).ready(function() {
-	clipboard.init();
+﻿$(document).ready(function () {
+	var client = new ZeroClipboard($("button#btnCopyToClipboard"));
 });
